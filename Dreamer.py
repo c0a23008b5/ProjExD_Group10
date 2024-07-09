0f77a5a40509a9a68adcde538fd6cb8b4f35b941
Yuyadayo <c0a23008b5@edu.teu.ac.jp>
2024-07-09 16:10:52 +0900

    小ボス全然完成してない

--- Dreamer.py
+++ Dreamer.py
@@ -1,18 +1,25 @@
 import os
 import sys
+import random
 import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+class small_enemy(pg.game.sprite.Sprite):
+    """
+    小ボスに関するクラス
+    """
+    
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像
+    bg_img = pg.image.load("fig/background1.jpg") #背景画像
     bg_img2 = pg.transform.flip(bg_img, True, False) #背景画像
-    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.image.load("fig/koukaton.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 0, 0.3)
     kk_rect = kk_img.get_rect() #こうかとんrectの抽出
     kk_rect.center = 300, 200
     tmr = 0
